0df72bf1286255700b688de123321aa1d652a87a
AbhayShaw1 <abhayshaw01ab@gmail.com>
2025-07-26 10:55:55 +0530

    Leetcode : ValindParenthesis

---
+++ Leetcode/validParenthesis.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    const mappings = {
+    ")": "(",
+    "}": "{",
+    "]": "[",
+  };
+
+  const stack  = [];
+
+  for (let c of s) {
+    if (mappings[c]) {
+      const topElement = stack.length ? stack.pop() : "#";
+
+      if (topElement !== mappings[c]) {
+        return false;
+      }
+    } else {
+      stack.push(c);
+    }
+  }
+
+  return stack.length === 0;
+};
